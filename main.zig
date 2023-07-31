2aad887590711537e4ac6d0ea55251c411ecd3ce
ben lim <lim6112j@gmail.com>
2023-07-31 11:54:48 +0900

    more subject

--- main.zig
+++ main.zig
@@ -35,6 +35,90 @@ test "if statement expression" {
     x += if (aa) 1 else 2;
     try expect(x == 1);
 }
+//while
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+}
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
+}
+test "for" {
+    const string = [_]u8{'h','e','l','l','o'};
+    for (string[0..]) |character, index| {
+        _ = character;
+        _ = index;
+    }
+    for (string)|character| {
+        _ = character;
+    }
+    for (string[0..]) |_,index| {
+        _ = index;
+    }
+    for (string) |_| {}
+}
+fn addFive(x: u32) u32 {
+    return x + 5;
+}
+test "functions" {
+    const y = addFive(0);
+    try expect(@TypeOf(y) == u32);
+    try expect(y == 5);
+}
+
+fn fibo(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibo(n-1) + fibo(n-2);
+}
+
+test "function recursion" {
+    const x = fibo(10);
+    try expect(x == 55);
+}
+
+test "defer" {
+    var x: i16 = 5;
+    {
+        defer x += 2;
+        try expect(x == 5);
+    }
+    try expect(x == 7);
+}
+
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    try expect(x == 4.5);
+}
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
 }
