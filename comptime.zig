dc2f413f7288cf703289371aa2661d902a60f0cb
ben <Lim6112j@gmail.com>
2025-04-07 20:23:54 +0900

    ...

--- comptime.zig
+++ comptime.zig
@@ -47,13 +47,22 @@ fn apply_ops(comptime operations: []const Op, num: i64) i64 {
     }
     return acc;
 }
-// Generic functions
-pub fn eql(comptime T: type, a: []const T, b: []const T) bool {
-    if (a.len != b.len) return false;
-    for (a) |item, index| {
-        if (b[index] != item) return false;}
-    return true;
-}
+
+const math = std.math;
+const assert = std.debug.assert;
+const Point = struct {
+    x: f64,
+    y: f64,
+    z: f64,
+    const Self = @This();
+    pub fn distance(self: *Self, p: Point) f64 {
+        const x2 = math.pow(f64, self.x - p.x, 2);
+        const y2 = math.pow(f64, self.y - p.y, 2);
+        const z2 = math.pow(f64, self.z - p.z, 2);
+        return math.sqrt(x2 + y2 + z2);
+    }
+};
+
 pub fn main() void {
     const len = comptime multiply(4, 5);
     const my_static_array: [len]u8 = undefined;
@@ -64,4 +73,8 @@ pub fn main() void {
     const ops = [4]Op{ .Sum, .Mul, .Sub };
     const y = apply_ops(ops[0..], user_num);
     std.debug.warn("Result: {}\n", y);
+    const p1 = Point{ .x = 0, .y = 2, .z = 8 };
+    const p2 = Point{ .x = 0, .y = 6, .z = 8 };
+    assert(p1.distance(p2) == 10);
+    assert(Point.distance(p1, p2) == 4);
 }
